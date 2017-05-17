b31c2d6ec006c7944f905ce537207405304d5d80
NAdamo <adam.nemeth@dpc.hu>
2017-05-17 14:49:16 +0200

    Primitives

---
+++ src/primitives.ts
@@ -0,0 +1,11 @@
+let isDone:boolean = true
+let hexNumber: number = 0xABAB;
+let binaryNumber:  number = 0b01101101;
+let octalNumber: number = 0o0734731;
+
+let message = `isDone: ${isDone}
+hexNumber: ${hexNumber}
+binaryNumber: ${binaryNumber}
+octalNumber: ${octalNumber}`
+
+console.log(message);
