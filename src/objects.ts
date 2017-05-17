40ec60f875822477ab43f6fe00f37880a74a77c0
NAdamo <adam.nemeth@dpc.hu>
2017-05-17 14:50:21 +0200

    Objects

---
+++ src/objects.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by nadamo on 2017. 05. 16..
+ */
+let myPoint2D: {x: number, y: number} = { x: 234, y: 234};
+console.log(myPoint2D);
+let myPoint3D: {x:number, y: number, z:number} = {x:23, y:24, z:25}
+console.log(myPoint3D);
+let myPoint: {x: number, y: number, z?:number} = {x: 26, y: 27};
+console.log(myPoint);
