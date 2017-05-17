db7540cef9938e750bdf6d1f9e0aecfe874bc26c
NAdamo <adam.nemeth@dpc.hu>
2017-05-17 14:49:37 +0200

    Enums

---
+++ src/enum.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by nadamo on 2017. 05. 16..
+ */
+enum Color{Red, Green, Blue}
+var myColor = Color.Green;
+console.log(myColor);
+var myColorName = Color[0];
+console.log(myColorName);
